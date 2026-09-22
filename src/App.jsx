01c50fbe27d0455502715cbf7dd5fced026b9069
CloudyClause <bruanjung@gmail.com>
2026-09-22 11:35:39 +0900

    Community페이지 기본화면 구성

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Community from './pages/Community'
 
 function App() {
 
   return (
     <>
     <h1>저희 쇼핑몰입니다.</h1>
+    <Community></Community>
     </>
   )
 }
