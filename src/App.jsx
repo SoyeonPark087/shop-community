4a92b47c5f8cd98b06c663836727d648f1e94662
SoyeonPark087 <010wlswn@gmail.com>
2026-09-23 14:58:19 +0900

    feat: shop 페이지 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import Shop from './pages/Shop'
-import ProductDetail from './pages/ProductDetail'
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import Home from './pages/Home'
 import './App.css'
 
+import Shop from './pages/Shop.jsx'
+
 function App() {
-  const pathname = window.location.pathname
-
-  // /shop/1, /shop/2처럼 상품 번호가 있는 경우
-  const isProductDetail = /^\/shop\/[^/]+\/?$/.test(pathname)
-
-  if (isProductDetail) {
-    return <ProductDetail />
-  }
-
-  // 기본 화면과 /shop은 상품 목록 표시
-  return <Shop />
+  
+  return (
+    <>
+      <Home />
+    </>
+  )
 }
 
 export default App
